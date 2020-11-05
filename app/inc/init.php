f62b9b7a1706f1ed980a45ffd43b1c9aa80a7478
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-11-05 18:46:35 +0100

    Restore real data

--- app/inc/init.php
+++ app/inc/init.php
@@ -6,8 +6,7 @@ require_once "{$root_folder}/vendor/autoload.php";
 // Cache class
 if (! defined('CACHE_ENABLED')) {
     // Allow disabling cache via config
-    // TODO: reset to true
-    define('CACHE_ENABLED', false);
+    define('CACHE_ENABLED', true);
 }
 define('CACHE_PATH', "{$root_folder}/cache/");
 define('CACHE_TIME', 60 * 60 * 6); // 6 hours
