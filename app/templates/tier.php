f35f62c25a63ae55c3fceb2b64561587d20fd736
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-11-05 17:22:16 +0100

    Add risk section

--- app/templates/tier.php
+++ app/templates/tier.php
@@ -1,3 +1,27 @@
+<h2>Locales at risk</h2>
+<p>Increasing number of missing strings:
+<?php
+  if (empty($locales_risk['missing'])) {
+    echo('none');
+  } else {
+    foreach ($locales_risk['missing'] as $locale) {
+      echo "<a href='?locale={$locale}&timeframe={$requested_timeframe}'>{$locale}</a> ";
+    }
+  }
+?>
+</p>
+<p>Increasing number of pending suggestions:
+ <?php
+  if (empty($locales_risk['suggestions'])) {
+    echo('none');
+  } else {
+    foreach ($locales_risk['suggestions'] as $locale) {
+      echo "<a href='?locale={$locale}&timeframe={$requested_timeframe}'>{$locale}</a> ";
+    }
+  }
+?>
+</p>
+
 <h2>Locales data</h2>
 <table class="table table-bordered" id="tier_details">
   <thead>
